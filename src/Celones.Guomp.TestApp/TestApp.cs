ef187d2364b637fec38ada5db9be174fe697b45c
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-02-22 23:27:47 +0100

    TestApp: Set font

--- src/Celones.Guomp.TestApp/TestApp.cs
+++ src/Celones.Guomp.TestApp/TestApp.cs
@@ -1,16 +1,22 @@
 ﻿using SkiaSharp;
 
-Celones.Device.Pcd8544 pcd = new(null, null, 0, 0);
-Celones.Guomp.Display.Nokia5110 display = new(pcd, null);
+var pcd = new Celones.Device.Pcd8544(null, null, 0, 0);
+var display = new Celones.Guomp.Display.Nokia5110(pcd, null);
 
 var paint = new SKPaint
 {
-    IsAntialias = false,
-    IsAutohinted = true
+    IsAntialias = false
+};
+
+var font = new SKFont
+{
+    Typeface = SKTypeface.FromFamilyName("Tahoma", SKFontStyleWeight.Bold, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright),
+    Edging = SKFontEdging.Alias,
+    Size = 10
 };
 
 display.Clear();
 display.Canvas.DrawLine(new SKPoint(5, 5), new SKPoint(60, 20), paint);
-display.Canvas.DrawText("Lorem Ipsum", new SKPoint(5, 30), paint);
+display.Canvas.DrawText("Lorem Ipsum", 5, 30, font, paint);
 
 display.SaveScreenshot("screenshot.png");
