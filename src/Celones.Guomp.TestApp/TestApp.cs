8ef2eac70e0b40330b96a0483f52c234dd2fdcbf
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-02-22 23:01:22 +0100

    TestApp: Add test application

---
+++ src/Celones.Guomp.TestApp/TestApp.cs
@@ -0,0 +1,16 @@
+﻿using SkiaSharp;
+
+Celones.Device.Pcd8544 pcd = new(null, null, 0, 0);
+Celones.Guomp.Display.Nokia5110 display = new(pcd, null);
+
+var paint = new SKPaint
+{
+    IsAntialias = false,
+    IsAutohinted = true
+};
+
+display.Clear();
+display.Canvas.DrawLine(new SKPoint(5, 5), new SKPoint(60, 20), paint);
+display.Canvas.DrawText("Lorem Ipsum", new SKPoint(5, 30), paint);
+
+display.SaveScreenshot("screenshot.png");
