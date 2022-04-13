cb2424e1bcdfccb855d98f0f37b8dcdba0f485d6
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-04-14 01:06:22 +0200

    TestApp: Create a subdirectory for screencaps

--- src/Celones.Guomp.TestApp/TestApp.cs
+++ src/Celones.Guomp.TestApp/TestApp.cs
@@ -12,4 +12,7 @@ var ui = new Ui("TestApp.xaml", display);
 
 ui.Show();
 
-display.Capture("capture.png");
+if (!Directory.Exists("capture"))
+    Directory.CreateDirectory("capture");
+
+display.Capture(string.Format("capture/{0:yyyy}{0:MM}{0:dd}_{0:HH}{0:mm}{0:ss}.{0:fff}.png", DateTime.Now));
