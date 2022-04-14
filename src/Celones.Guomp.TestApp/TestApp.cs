369914cf1c48745d8a44a70d4e8fce1e7bf760a7
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-04-14 02:04:18 +0200

    TestApp: Add basic menu item selection

--- src/Celones.Guomp.TestApp/TestApp.cs
+++ src/Celones.Guomp.TestApp/TestApp.cs
@@ -1,4 +1,5 @@
 ﻿using Celones.Guomp;
+using Celones.Guomp.Controls;
 using Celones.Guomp.Displays;
 using Celones.Guomp.TestApp;
 
@@ -10,9 +11,31 @@ var pcd = new Celones.Device.Pcd8544(spi, gpio, 0, 0);
 var display = new Nokia5110(pcd, pwm);
 var ui = new Ui("TestApp.xaml", display);
 
-ui.Show();
-
 if (!Directory.Exists("capture"))
     Directory.CreateDirectory("capture");
 
-display.Capture(string.Format("capture/{0:yyyy}{0:MM}{0:dd}_{0:HH}{0:mm}{0:ss}.{0:fff}.png", DateTime.Now));
+while (true)
+{
+    ui.Show();
+    display.Capture(string.Format("capture/{0:yyyy}{0:MM}{0:dd}_{0:HH}{0:mm}{0:ss}.{0:fff}.png", DateTime.Now));
+
+    var menu = ui.Root.Items.OfType<Menu>().First();
+    var index = menu.Items.IndexOf(menu.SelectedItem);
+
+    // ReSharper disable once SwitchStatementMissingSomeEnumCasesNoDefault
+    switch (Console.ReadKey().Key)
+    {
+        case ConsoleKey.Escape:
+            return;
+
+        case ConsoleKey.UpArrow:
+            if (index != 0)
+                menu.SelectedItem = (MenuItem)menu.Items[index - 1];
+            break;
+
+        case ConsoleKey.DownArrow:
+            if (index != (menu.Items.Count - 1))
+                menu.SelectedItem = (MenuItem)menu.Items[index + 1];
+            break;
+    }
+}
